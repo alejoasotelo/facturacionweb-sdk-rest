c099b571e599791e1b8f9b3253c556b52e027e54
Alejo Sotelo <alejoasotelo@outlook.com>
2020-07-15 18:59:31 -0300

    [+] Agregué las provincias

---
+++ src/provincias.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AlejoASotelo\FacturacionWeb;
+
+class Provincias
+{
+    const BUENOS_AIRES = 0;
+    const CATAMARCA = 1;
+    const CHACO = 2;
+    const CHUBUT = 3;
+    const CORDOBA = 4;
+    const CORRIENTES = 5;
+    const ENTRE_RIOS = 6;
+    const FORMOSA = 7;
+    const JUJUY = 8;
+    const LA_PAMPA = 9;
+    const LA_RIOJA = 10;
+    const MENDOZA = 11;
+    const MISIONES = 12;
+    const NEUQUÉN = 13;
+    const RIO_NEGRO = 14;
+    const SALTA = 15;
+    const SAN_JUAN = 16;
+    const SAN_LUIS = 17;
+    const SANTA_CRUZ = 18;
+    const SANTA_FE = 19;
+    const SANTIAGO_DEL_ESTERO = 20;
+    const TIERRA_DEL_FUEGO = 21;
+    const TUCUMAN = 22;
+    const CAPITAL_FEDERAL = 23;
+}
